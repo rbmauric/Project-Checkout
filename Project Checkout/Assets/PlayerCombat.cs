981596a8dde2e2706f412758b0671fb13286e9b3
rbmauric <35748166+rbmauric@users.noreply.github.com>
2020-02-28 14:15:42 -0800

    added player attack functionality and basic enemy class

---
+++ Project Checkout/Assets/PlayerCombat.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public int attackDamage = 40;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public float attackRate = 0.5f;
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //Play Attack Animation
+        animator.SetTrigger("Attack");
+        //Check if enemy is in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //Damage enemy
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+            enemy.GetComponent<Enemy>().takeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);    
+    }
+}
