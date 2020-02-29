fa4a12806c21b40efcea002e4f3162b22273dd20
rbmauric <35748166+rbmauric@users.noreply.github.com>
2020-02-28 16:05:12 -0800

    added item source code

---
+++ Project Checkout/Assets/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Item!");
+            other.GetComponent<PlayerStats>().items.Add(gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
