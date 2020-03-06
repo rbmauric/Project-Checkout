2aa5391d6eabb3949b677895359653ac02e3a17d
magRubennet <47723008+magRubennet@users.noreply.github.com>
2020-03-06 13:01:03 -0800

    Now Health Bar Complete
    Need only scaling now.

--- Project Checkout/Assets/Scenes/HealthBar.cs
+++ Project Checkout/Assets/Scenes/HealthBar.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using UnityEngine.UI;  //??Outdated??
-using UnityEngine.UIElements;  // New?
+using UnityEngine.UI;  //??Outdated??
+//using UnityEngine.UIElements;  // New?
 
 public class HealthBar : MonoBehaviour
 {
@@ -14,10 +14,10 @@ public class HealthBar : MonoBehaviour
     //Setting Max Health
     public void SetMaxHealth(int health) 
     {
-    //    slider.maxValue = health;   //Suppose to set max health, but maxValue isn't a definiton?
+        slider.maxValue = health;   //Suppose to set max health, but maxValue isn't a definiton?
         slider.value = health;
 
-     //   fill.color = gradient.Evaluate(1f);  //Set Max Health to color on the right
+        fill.color = gradient.Evaluate(1f);  //Set Max Health to color on the right
     }
     
     //Setting health slider
@@ -25,7 +25,7 @@ public class HealthBar : MonoBehaviour
     {
         slider.value = health;
 
-       // fill.color = gradient.Evaluate(slider.normalizedValue);
+        fill.color = gradient.Evaluate(slider.normalizedValue);
        // Update Health Bar color to whatever value the slider has
     }
 
--- Project Checkout/Assets/Scenes/Player.cs
+++ Project Checkout/Assets/Scenes/Player.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+// using UnityEngine.UIElements;
 
 
 
