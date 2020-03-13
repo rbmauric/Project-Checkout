2e502654c165f63aebb332c523846dbda142f4cb
magRubennet <47723008+magRubennet@users.noreply.github.com>
2020-03-13 13:49:44 -0700

    Timer and Ranged Enemy

---
+++ Project Checkout/Assets/Scenes/CountdownTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public Gradient gradient_text;
+
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    //    countdownText.color = gradient_text.Evaluate(startingTime); 
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        countdownText.color = gradient_text.Evaluate(currentTime); //
+
+        if (currentTime <= 0)
+        {
+            //Kinda suppose to deactivate the timer when certain requiement met 
+            //Possible to use later on
+            //  CountdownText.gameobject.SetActive(false); 
+            currentTime = 0;
+        }
+        //   if(currentTime>=3.5f){CountDownText.color = Color.black;}
+        //   if (currentTime < 3.5f) { CountDownText.color = Color.red; }
+        // For reference
+
+    }
+}
---
+++ Project Checkout/Assets/Scenes/RangedEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject Projectile;
+
+    public Transform GroundCheck;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(Projectile, GroundCheck);
+    }
+}
