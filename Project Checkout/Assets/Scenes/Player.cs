dd3c51aa1565197de2790ed407b81043d016344c
magRubennet <47723008+magRubennet@users.noreply.github.com>
2020-03-26 12:18:54 -0700

    So Far

--- Project Checkout/Assets/Scenes/Player.cs
+++ Project Checkout/Assets/Scenes/Player.cs
@@ -14,13 +14,38 @@ public class Player : MonoBehaviour
 
     public HealthBar healthBar;
 
+    //------------------------------------
+    public float speed = 5;
+
+    public float jumpForce;
+
+    [HideInInspector]
+    public Rigidbody2D playerRB;
+
+    private float movementInput;
+
+    //------------------------------------
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
         //Setting the Player health to max
+        //----------------------------------
+        playerRB = GetComponent<Rigidbody2D>();
     }
+
+    //-------------------------------------------
+    private void FixedUpdate()
+    {
+        movementInput = Input.GetAxisRaw("Horizontal");
+        //Horizontal Input
+
+        playerRB.velocity = new Vector2(movementInput * speed, playerRB.velocity.y);
+
+    }
+    //-------------------------------------------
 
     // Update is called once per frame
     void Update()
@@ -30,9 +55,22 @@ public class Player : MonoBehaviour
         {
             TakeDamage(20);
         }
+        //-------------------------------
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
 
 
     }
+
+    //------------------------------------
+    void Jump()
+    {
+        playerRB.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+    }
+    //------------------------------------
 
     //The damage subtraction
     void TakeDamage(int damage)
--- Project Checkout/Assets/Scenes/RangedEnemy.cs
+++ Project Checkout/Assets/Scenes/RangedEnemy.cs
@@ -5,29 +5,67 @@ using UnityEngine;
 public class RangedEnemy : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject Projectile;
-
+    public GameObject Projectile;
     public Transform GroundCheck;
+    public float timeBetweenAttacks = 1.5f;
+
+    [HideInInspector]
+    public BoxCollider2D ranged;
+
+   // Player playerRB;
+
+    bool playerInRange;
+    float timer;
+    GameObject player;
+  //  Player player;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        ranged = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && playerInRange != false)
         {
             Shoot();
+        }
+        //---------------------------------------
+ /*       if(playerRB < ranged)
+        {
+            Shoot();
+        }
+        */
+
+    }
+
+    void OnTriggerEnter(Collider enemy)
+    {
+        if (enemy.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider enemy)
+    {
+        if (enemy.gameObject == player)
+        {
+            playerInRange = false;
         }
     }
 
     void Shoot()
     {
+        timer = 0f;
         Instantiate(Projectile, GroundCheck);
     }
 }
