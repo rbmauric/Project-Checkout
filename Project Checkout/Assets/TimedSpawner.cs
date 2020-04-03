8d01e124fe88db86956a6c1eb310fe9da9efe7c2
JohnDinh2 <54484047+JohnDinh2@users.noreply.github.com>
2020-04-02 20:45:29 -0700

    Added Spawn Timer

---
+++ Project Checkout/Assets/TimedSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpawner : MonoBehaviour
+{
+    public GameObject go;//the object to spawn
+    public bool stopper = false;//to make the spawner stop spawning
+    public float timer;//how long it takes an object to spawn
+    public float cooldown;//how long before the next object spawns
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnObj", timer, cooldown);
+    }
+
+    public void spawnObj()
+    {
+        Instantiate(go, transform.position, transform.rotation);
+        if(stopper)
+        {
+            CancelInvoke("spawnObj");
+        }
+    }
+}
