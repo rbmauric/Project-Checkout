c396dbaf5b36cc37dfc9c5c21c0d77c9f63c62aa
magRubennet <47723008+magRubennet@users.noreply.github.com>
2020-04-10 00:24:35 -0700

    Everything almodt done
    Just need to move the enemy's projectile.

---
+++ Project Checkout/Assets/_Scripts/EnemyMeleeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMeleeTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D attack)
+    {
+        if (attack.CompareTag("Player"))
+        {
+            attack.GetComponent<Player>().Damage();
+        }
+    }
+}
---
+++ Project Checkout/Assets/_Scripts/EnemyRangeTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangeTrigger : MonoBehaviour
+{
+  /*
+
+    private void OnTriggerEnter2D(Collider2D attack)
+    {
+        
+        if (attack.CompareTag("Player"))
+        {
+            // attack.GetComponent<Player>().Damage();
+            attack.GetComponent<RangeEnemy>().RangeTrigger();
+            attack.GetComponent<RangeEnemy>().Shoot();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D _OoB)
+    {
+        if (_OoB.CompareTag("Player"))
+        {
+            _OoB.GetComponent<RangeEnemy>().OoBTrigger();
+        }
+    }
+    */
+}
